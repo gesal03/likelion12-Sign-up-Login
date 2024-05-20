8865b6001903900bc9e75f3f08a8b0a61ffa631f
이현승 <77336664+gesal03@users.noreply.github.com>
2024-05-20 10:21:03 +0900

    🩹 Change Phone regular expression

--- account-assignment-base-main/src/main/java/com/example/account/account/dto/SignUpDto.java
+++ account-assignment-base-main/src/main/java/com/example/account/account/dto/SignUpDto.java
@@ -24,7 +24,7 @@ public class SignUpDto {
         private String passwordCheck;
         @Email(message = "Email 형식이 맞지 않습니다.")
         private String email;
-        @Pattern(regexp = "^010-\\d{3,4}-\\d{4}$", message = "전화번호 형식이 맞지 않습니다.")
+        @Pattern(regexp = "^010\\d{8}$", message = "전화번호 형식이 맞지 않습니다.")
         private String phone;
         public Member toEntity() {
             return Member.builder()
